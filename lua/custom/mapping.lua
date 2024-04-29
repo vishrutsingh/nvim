4822cf9dff4990da34dda0a5ad97c3a1a3393c60
Vishrut Singh <vishrutsingh@Vishruts-MacBook-Air.local>
2024-04-29 22:22:16 +0530

    add gitsigns

--- lua/custom/mapping.lua
+++ lua/custom/mapping.lua
@@ -7,7 +7,7 @@ vim.opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 vim.opt.shiftwidth = 2 -- 2 spaces for indent width
 vim.opt.expandtab = true -- expand tab to spaces
 vim.opt.autoindent = true -- copy indent from current line when starting new one
-vim.opt.relativenumber = true
+vim.o.relativenumber = true
 vim.opt.wrap = false
 
 --search
