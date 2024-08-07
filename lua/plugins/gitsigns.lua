ff64b3b8dc0dc8a164c4448c7033ac87de267cab
Vishrut Singh <vishrutsingh@Vishruts-MacBook-Air.local>
2024-08-08 00:09:38 +0530

    Git

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,13 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
   opts = {
     on_attach = function(bufnr)
       local gs = package.loaded.gitsigns
